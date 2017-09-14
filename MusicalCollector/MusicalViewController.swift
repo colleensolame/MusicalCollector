472c7805ebe516d6642a3bfe9c22f54ff1c8fcea
Colleen Ng <colleenkyng@gmail.com>
2017-09-14 14:37:21 +0800

    Inserting buttons and textfields and segues to view controller

---
+++ MusicalCollector/MusicalViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MusicalViewController.swift
+//  MusicalCollector
+//
+//  Created by Colleen Ng on 9/14/17.
+//  Copyright © 2017 ZND Code. All rights reserved.
+//
+
+import UIKit
+
+class MusicalViewController: UIViewController {
+
+    
+    @IBOutlet weak var musicalImageView: UIImageView!
+    @IBOutlet weak var txtTitle: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnCamera(_ sender: Any) {
+    }
+    
+    @IBAction func btnPhotos(_ sender: Any) {
+    }
+    
+    @IBAction func btnAdd(_ sender: Any) {
+    }
+    
+}
